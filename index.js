6d786852938a00e5d6b4b4fec33430197609c8e0
nguyenkhanh0310 <mykhanh03102003@gmail.com>
2025-02-06 21:33:08 +0700

    Create structure base

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const express = require('express');
+require('dotenv').config();
+
 const app = express();
+const port = process.env.PORT;
+
+const route = require("./routes/client/index.route");
+
+// Route
+route(app);
+
+// View pug
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
